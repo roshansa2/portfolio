4b60215ea1a2c14468784d145110942b3bf7cfd9
roshansa2 <sagopalji@gmail.com>
2024-08-28 16:47:54 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@
 
 /* ----- TYPING EFFECT ----- */
    var typingEffect = new Typed(".typedText",{
-      strings : ["Designer","Youtuber","Developer"],
+      strings : ["Designer","Marketer","Developer"],
       loop : true,
       typeSpeed : 100, 
       backSpeed : 80,
